c6becc7c2d914fe6e2a39fdd924d82504733e8a9
Patrice Chalin <pchalin@gmail.com>
2017-03-15 09:20:02 -0700

    resetHeroes method tweaks

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2017, chalin. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
@@ -14,13 +16,17 @@ import 'hero.dart';
   providers: const [materialProviders],
 )
 class AppComponent implements OnInit {
-  List<Hero> heroes;
+  final List<Hero> heroes = [];
   Hero currentHero;
 
   void ngOnInit() => resetHeroes();
 
   void resetHeroes() {
-    heroes = Hero.mockHeroes.map((hero) => new Hero.copy(hero)).toList();
+    heroes.clear();
+    Hero.mockHeroes.forEach((hero) => heroes.add(new Hero.copy(hero)));
     currentHero = heroes[0];
+    // https://github.com/dart-lang/angular2_components/issues/61
+    // Test workaround:
+    Timer.run(() {currentHero = heroes[0];});
   }
 }
